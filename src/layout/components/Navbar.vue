07b22a5635770bc7cc867551db90ed2f876ccf20
Alex Chen <Zhenke.18@intl.zju.edu.cn>
2021-08-06 21:41:40 +0800

    404 bug fixed

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -82,7 +82,7 @@ export default {
     },
     async logout() {
       await this.$store.dispatch('user/logout')
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      this.$router.push(`/login?redirect=homepage`)
     }
   }
 }
